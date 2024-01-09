d2529cb98035e68f46b7ff5594cbe0d8ded8f388
Hacker07xtt <141055210+Hacker07xtt@users.noreply.github.com>
2024-01-09 14:13:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,23 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("27266473"))
+API_HASH = getenv("3455697a8c977355bdf136d80cf8b145")
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("6052374952:AAG7nuIrbZFBPWXG7f8XkZ2j0mIJAXsLxYs", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("2070450796"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/84819fc115cb0eff32b2b.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tbcbotschat")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tbc_bots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MUSICxcRobot")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5090817443").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5852269476").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
